2a8652fac6b9e7b38721780c0856d622a99c2927
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-10 01:30:22 +0700

    step 3

--- Basics.py
+++ Basics.py
@@ -17,7 +17,13 @@ faceLocTest = face_recognition.face_locations(imgTest)[0]
 encodeAdiTest = face_recognition.face_encodings(imgTest)[0] # encode the image
 cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 255, 0), 2)
 
-
+#! step 3, compare the encoding (128 measurements of both the faces)
+# at the backend using linear SVM to find out match or not
+results = face_recognition.compare_faces([encodeAdi], encodeAdiTest)
+# can see the distance of similarity
+faceDis = face_recognition.face_distance([encodeAdi], encodeAdiTest) # bisa digunakan saat ingin menggunakan train foto yg paling mirip
+print(results, faceDis)
+cv2.putText(imgTest, f'{results[0]} {round(faceDis[0], 2)}', (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
 
 cv2.imshow('Adipati M.A.', imgAdi)
