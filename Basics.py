049280bb3d027bdd6ce538f0a74f22ef9a9277ef
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-10 01:30:08 +0700

    step 2

--- Basics.py
+++ Basics.py
@@ -8,6 +8,15 @@ imgAdi = cv2.cvtColor(imgAdi, cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('ImageBasic/nindya2.jpg')
 imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
+#! step 2, locate the proper face location
+faceLoc = face_recognition.face_locations(imgAdi)[0]
+encodeAdi = face_recognition.face_encodings(imgAdi)[0] # encode the image
+cv2.rectangle(imgAdi, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 255, 0), 2)
+
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeAdiTest = face_recognition.face_encodings(imgTest)[0] # encode the image
+cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 255, 0), 2)
+
 
 
 
